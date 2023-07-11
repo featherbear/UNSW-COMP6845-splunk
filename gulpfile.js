62d36ae6078ce5990c6a593277dddb3b3149ad93
Andrew Wong <andrew.j.wong@outlook.com>
2023-07-11 10:12:30 +1000

    fix base

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 const { watch, src, dest, task, symlink, parallel, series } = require('gulp')
 const newer = require('gulp-newer')
 
-function setupCopier(srcGlob, destPath) {
+function setupCopier(srcGlob, destPath, onlyCheckNewer) {
     let runner = function (next) {
-        src(srcGlob)
-            .pipe(newer(destPath))
-            .pipe(dest(destPath))
+        let pipe = src(srcGlob)
+        if (!onlyCheckNewer) pipe = pipe.pipe(newer(destPath))
+        pipe.pipe(dest(destPath))
         return next()
     }
     runner.displayName = "Copy " + srcGlob
@@ -16,7 +16,7 @@ function setupWatcher(srcGlob, destPath) {
     return watch(
         srcGlob,
         { ignoreInitial: false },
-        setupCopier(srcGlob, destPath)
+        setupCopier(srcGlob, destPath, true)
     )
 }
 
@@ -37,15 +37,17 @@ exports.watch = task('watch', () => {
     setupWatcher('content/recordings/**', 'base/public/asciinema/recordings/')
     setupWatcher('content/sections/images/**', 'base/public/images/')
     setupWatcher('content/sections/*.svx', 'base/src/sections/')
-    setupWatcher('content/pages.ts', 'base/src/pages.ts')
+    setupWatcher('content/pages.ts', 'base/src/')
+    setupWatcher('content/index.html', 'base/')
 })
 
 exports.copy = task('copy', series(
-    setupCopier('content/recordings/**', 'base/public/asciinema/recordings/'),
-    setupCopier('content/sections/images/**', 'base/public/images/'),
-    setupCopier('content/sections/*.svx', 'base/src/sections/'),
-    setupCopier('content/pages.ts', 'base/src/pages.ts'))
-)
+    setupCopier('content/recordings/**', 'base/public/asciinema/recordings/', false),
+    setupCopier('content/sections/images/**', 'base/public/images/', false),
+    setupCopier('content/sections/*.svx', 'base/src/sections/', false),
+    setupCopier('content/pages.ts', 'base/src/', false),
+    setupCopier('content/index.html', 'base/', false)
+))
 
 exports.default = parallel('watch', 'start')
 
